8cfad0df5ce83bff82a28cde1ab863783143fef3
Arshia Quraishi <arshia.quraishi@gmail.com>
2016-02-20 11:15:07 -0600

    Committing before uploading to GitHub

---
+++ Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  HomeInventory
+//
+//  Created by Hafeez on 1/28/16.
+//  Copyright © 2016 Arshia. All rights reserved.
+//
+
+import Foundation
---
+++ Views/TableViewCells/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  HomeInventory
+//
+//  Created by Hafeez on 1/29/16.
+//  Copyright © 2016 Arshia. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
